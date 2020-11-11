fc1df8d1b316b99bb966e57f29ca884064cc1a01
vdElin <amnel-gfx@hotmail.com>
2020-11-11 19:41:16 +0100

    Simplification de la doc

--- src/arbre_binaire/Sommet.h
+++ src/arbre_binaire/Sommet.h
@@ -1,32 +1,34 @@
 #ifndef SOMMET_H
 #define SOMMET_H
+
 #include <iostream>
 
-
-//class sommet :
-    //1) On met les attributs en protégé pour que la class ArbreB puisse avoir directement accès à ces derniers
-    //2) Le premier constructeur initie des valeurs par défaut afin de ne pas avoir à créer un autre constructeur
-           //sans paramètres
-    //3) Il y aussi le constructeur par recopie qui est implémenté
-    //4) On déclare aussi des accesseurs et des méthodes pour modifier les attributs de manière à respecter
-        //l'encapsulation
-    //5) On redéfinit les opérateurs +, = et <<
-
-    //Ps : les accesseurs et les setteurs sont inline car ce sont des méthodes assez basiques
-class Sommet
-{
+/*
+ * Classe Sommet :
+ *    Les attributs sont marqués `protected` pour que la Class fille ArbreB puisse avoir accès à ces derniers
+ *          @lettre    représente un caractère du texte à analyser
+ *          @freq      représente la fréquence d'occurrence de ce caractère
+ *    Constructeurs:
+ *          1. Constructeur par défaut avec valeurs initialisés si aucun paramètres
+ *          2. Constructeur par copie
+ *    Accesseurs (Getters) et modificateurs (Setters) implémentés pour le respect de l'encapsulation
+ *    Surcharges des opérateurs +, = et <<
+ */
+ 
+class Sommet {
     protected :
         char lettre;
         float freq;
+
     public:
         Sommet(const char =' ',const float = 0);
         Sommet(Sommet &);
         ~Sommet();
 
-        char getLettre() {return lettre;};
-        float getFreq() {return freq;};
-        void setLettre(const char car) {lettre = car;};
-        void setFreq(const float part) {freq = part;};
+        char  getLettre()              { return lettre; };
+        float getFreq()                { return freq;   };
+        void setLettre(const char car) { lettre = car;  };
+        void setFreq(const float part) { freq   = part; };
 
         void operator=(Sommet &);
         friend Sommet operator+(Sommet & ,Sommet &);
