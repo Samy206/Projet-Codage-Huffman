9444cae087fc601d3e428c6db59a9b45a3c6c752
Samy206 <samy.boumali@hotmail.com>
2020-11-11 13:00:09 +0100

    Pitiiiit pb destructeur

--- src/arbre_binaire/Sommet.h
+++ src/arbre_binaire/Sommet.h
@@ -2,20 +2,31 @@
 #define SOMMET_H
 #include <iostream>
 
+
+//class sommet :
+    //1) On met les attributs en protégé pour que la class ArbreB puisse avoir directement accès à ces derniers
+    //2) Le premier constructeur initie des valeurs par défaut afin de ne pas avoir à créer un autre constructeur
+           //sans paramètres
+    //3) Il y aussi le constructeur par recopie qui est implémenté
+    //4) On déclare aussi des accesseurs et des méthodes pour modifier les attributs de manière à respecter
+        //l'encapsulation
+    //5) On redéfinit les opérateurs +, = et <<
+
+    //Ps : les accesseurs et les setteurs sont inline car ce sont des méthodes assez basiques
 class Sommet
 {
     protected :
         char lettre;
-        int freq;
+        float freq;
     public:
-        Sommet(const char =' ',const int = 0);
+        Sommet(const char =' ',const float = 0);
         Sommet(Sommet &);
         ~Sommet();
 
         char getLettre() {return lettre;};
-        int getFreq() {return freq;};
+        float getFreq() {return freq;};
         void setLettre(const char car) {lettre = car;};
-        void setFreq(const int part) {freq = part;};
+        void setFreq(const float part) {freq = part;};
 
         void operator=(Sommet &);
         friend Sommet operator+(Sommet & ,Sommet &);
--- src/arbre_binaire/TestArbre.cc
+++ src/arbre_binaire/TestArbre.cc
@@ -1,17 +1,17 @@
 #include "ArbreB.cc"
 #include "Sommet.cc"
+#include <unistd.h>
 using namespace std;
 
 int main()
 {
-    srand(4);
-    const char cars[26] = {'a','b','c','d','e','f','j','h','i','g','k','l','m','n','o','p','q','r','s','t','u',
-        'v','w','x','y','z'};
+    srand(getpid());
+    const char cars[13] = {'a','b','c','d','e','f','j','h','i','g','k','l','m'};
 
-    int numbers [26];
-    Sommet table1 [26];
+    float numbers [13];
+    Sommet table1 [13];
 
-    for(int i = 0 ; i < 26 ; i++)
+    for(int i = 0 ; i < 13 ; i++)
     {
         numbers[i] = rand() % 100;
         Sommet * random = new Sommet(cars[i],numbers[i]);
@@ -19,15 +19,40 @@ int main()
         delete random;
     }
 
-
-
-    ArbreB arbre1(table1,26);
+    ArbreB arbre1(table1,13);
     cout <<arbre1<<"\n\n";
-    const char c = 'h';
-    arbre1.supprimer_noeud(c);
+    /*for(int i = 0 ; i < 6 ; i++)
+    {
+        cout<<"Suppression de la lettre "<<cars[i]<<" -----------------------------------------------------\n\n";
+        arbre1.supprimer_noeud(arbre1.getRacine(),cars[i]);
+        cout<<arbre1;
+    }*/
 
-    cout<<"\n\n"<<arbre1;
+    ArbreB arbre2(arbre1);
+    cout<<"affichage arbre 2 copie de arbre1 \n\n"<<arbre2;
 
+    const char recherche = 'j';
+    Noeud * res = arbre2.recherche_noeud(arbre2.getRacine(),recherche);
+
+    if(res != NULL)
+        cout<<"\n\nres : lettre-> '"<<res->actuel.getLettre()<<"' , frequence : "<<res->actuel.getFreq()<<endl;
+
+
+    Noeud * table2 = new Noeud[13];
+    for(int i = 0 ; i < 13 ; i++)
+    {
+        numbers[i] = rand() % 100;
+        Sommet * random = new Sommet(cars[i],numbers[i]);
+        table2[i].actuel = *random;
+        table2[i].filsg = NULL;
+        table2[i].filsd = NULL;
+        delete random;
+    }
+    ArbreB arbre3(table2,13);
+
+    cout<<"\nAffichage arbre3 , on l'initialise avec des variables de type Noeud\n\n"<<arbre3;
+    arbre3.change_etiquette(cars[2],19);
+    cout<<"\nChangement de la fréquence de c à 19\n\n"<<arbre3;
 }
 
 
